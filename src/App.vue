12c70c331bb8b46002cba4d61aed7a08319dbd51
Prakhar Maurya <000.excel@gmail.com>
2021-02-03 11:49:40 +0530

    initial vue image compressor

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p v-if="!hasImage">Loading....</p>
+    <image-uploader
+      :preview="true"
+      :sizeTo="200"
+      :capture="false"
+      :debug="1"
+      doNotResize="gif"
+      :autoRotate="false"
+      outputFormat="verbose"
+      @doneCompression="setImage"
+    >
+      <label for="fileInput" slot="upload-label">
+        <span class="upload-caption">{{
+          hasImage ? "Replace" : "Upload"
+        }}</span>
+      </label>
+    </image-uploader>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ImageUploader from "./components/ImageUploader.vue";
 
 export default {
-  name: 'App',
+  name: "app",
   components: {
-    HelloWorld
-  }
-}
+    ImageUploader,
+  },
+  data() {
+    return { hasImage: false };
+  },
+  methods: {
+    setImage: function(output) {
+      this.hasImage = true;
+      this.image = output;
+    },
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
